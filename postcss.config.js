dcc2d6382232b977406ef1142ed9d83a0e5f037c
garvitkumar854 <garvitkajot854@gmail.com>
2025-06-04 20:19:45 +0530

    Ready to Deployment 7

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-preset-env': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  },
+  }
 };
